43cb959aa0f3e5cd5bfa81df6095168691b5d5e5
Foxo <109620839+FoxoThatCat@users.noreply.github.com>
2024-05-25 21:47:30 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# FoxoHack
+
+This is where she makes a mod.
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+
+using namespace geode::prelude
+
+clss $modify(PlayLayer) {
+	void destroyPlayer(PlayerObject* player, GameObject object*) {
+	}
+};
